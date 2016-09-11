228c45787a1f1bd9303e302802e2827c59820e87
Farzad taslimi <farzaduss@gmail.com>
2016-09-11 19:28:56 +0430

    Facebook authentication added

--- socialApp/SigninVC.swift
+++ socialApp/SigninVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SigninVC: UIViewController {
 
@@ -20,6 +23,33 @@ class SigninVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnAction(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Farzad: facebook Login had nil : \(error)")
+            }else if result?.isCancelled == true {
+                print("Farzad: the user canceled : \(result)")
+            } else {
+                print("successfully signed in with facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+                
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Frazad: unable to authenticate with Firebase : \(error)")
+            } else {
+                print("Farzad: Successfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
